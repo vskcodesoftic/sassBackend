fd77c840f3988229b21ad70153f593f6fda506ac
vskcodesoftic <vegisiva@codesoftic.com>
2021-03-19 17:21:22 +0530

    added basic sass

---
+++ app.js
@@ -0,0 +1,59 @@
+require("dotenv").config()
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const HttpError = require('./models/httpError')
+
+const homepageRoutes = require('./routes/homepage-Routes')
+
+
+const app = express();
+
+//body parsing jsonData
+app.use(bodyParser.json())
+//routes
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
+
+
+
+app.use(homepageRoutes);
+
+
+app.use((req, res, next)=>{
+    const error = new HttpError('could not found this Route', 404);
+    throw error;
+})
+
+
+// error model middleware
+app.use((error, req, res, next) => {
+
+    if (res.headerSent) {
+      return next(error);
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occurred!'});
+  });
+
+  mongoose.connect(process.env.MONGO_PROD_URI,{  useNewUrlParser: true , useUnifiedTopology: true  ,'useCreateIndex' : true })
+  .then(() => {
+    console.log("server is live");
+    app.listen(process.env.PORT || 8001, function(){
+      console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    });;
+  
+  })
+  .catch(err => {
+    console.log(err);
+  });
